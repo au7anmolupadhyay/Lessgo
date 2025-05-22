ae2f2bc9563562eef2fadd7256ff08e836c8a7c2
Anmol Upadhyay <anmolupadhau7@gmail.com>
2025-05-23 01:34:03 +0530

    frontend started

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>
+    <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
+        <div className='bg-white'>
+          {/* <img src="/lessgo_homepage.jpg" alt="logo" /> */}
+          <h2 className='text-2xl font-bold'>Get Started with Lessgo!</h2>
+          <button className='m-2 w-full bg-black text-white p-2 rounded-md'>Continue</button>
+        </div>
+  </div>
+  </div>
+};
+
+export default Home;
