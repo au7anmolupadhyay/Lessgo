e05acd171f9a56bdf3ac31a07ee864728cde7e85
Anmol Upadhyay <anmolupadhau7@gmail.com>
2025-05-23 23:24:18 +0530

    edited home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>
-    <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
-        <div className='bg-white'>
-          {/* <img src="/lessgo_homepage.jpg" alt="logo" /> */}
-          <h2 className='text-2xl font-bold'>Get Started with Lessgo!</h2>
-          <button className='m-2 w-full bg-black text-white p-2 rounded-md'>Continue</button>
-        </div>
-  </div>
-  </div>
+  return (
+    <div className="h-screen w-screen overflow-hidden flex items-center justify-center">
+      <div className="bg-white max-w-md w-full p-4 rounded-md shadow-md text-center">
+        <img
+          src="/lessgo_homepage.jpg"
+          alt="logo"
+          className="w-full h-auto object-contain"
+        />
+        <h2 className="text-2xl font-bold mt-4">Get Started with Lessgo!</h2>
+        <Link to="/login" className="flex items-center justify-center mt-4 w-full bg-black text-white p-2 rounded-md">
+          Continue
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
