0b3ba6047d8d4026ee396b8501a8832244a7ca33
Anmol Upadhyay <anmolupadhau7@gmail.com>
2025-05-26 00:15:58 +0530

    added captain login

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const CaptainLogin = () => {
-  return <div>CaptainLogin</div>;
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [captainData, setCaptainData] = useState({});
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    setCaptainData({email:email, password:password});
+    console.log({email, password});
+
+    setEmail('');
+    setPassword('');
+  } 
+  return(
+    <form onSubmit={(e)=>submitHandler(e)}>
+      <div className="flex flex-col min-h-screen bg-gradient-to-b from-white to-gray-200">
+
+        <div className="p-4">
+          <img src="/lessgo_captain.png" alt="Lessgo Logo" className="h-12" />
+        </div>
+
+        <div className="px-6">
+          <div className="mb-4">
+            <label className="block text-gray-800 text-sm font-semibold mb-2">
+              What's your email?
+            </label>
+            <input
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="email@example.com"
+              className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 focus:outline-none"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-800 text-sm font-semibold mb-2">
+              Enter Password
+            </label>
+            <input
+              type="password"
+              required
+              value = {password}
+              onChange = {(e)=>setPassword(e.target.value)}
+              placeholder="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 focus:outline-none"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-2 rounded font-bold mb-3"
+          >
+            Login
+          </button>
+          <p className="text-center">
+            Join a fleet?
+            <Link to="/signup" className="text-blue-600 ml-2">Register as a Captain</Link>
+          </p>
+        </div>
+
+        <div className="mt-auto p-4">
+          <Link
+            to="/captain-signup"
+            className="block w-full bg-orange-500 text-white text-center py-2 rounded font-bold mb-5"
+          >
+            Sign in as User
+          </Link>
+        </div>
+      </div>
+    </form>
+  )
 };
 
 export default CaptainLogin;
