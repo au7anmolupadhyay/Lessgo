ba831675b0fcee092e715b2245eced777e22d042
Anmol Upadhyay <anmolupadhau7@gmail.com>
2025-05-31 12:14:52 +0530

    added logic for logout

--- frontend/src/pages/UserLogout.jsx
+++ frontend/src/pages/UserLogout.jsx
@@ -3,7 +3,28 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const UserLogout = () => {
+
     const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+
+    axios.get(`${import.meta.env.VITE_BASE_URL}/users/logout`,{
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    })
+    .then((res)=>{
+        if(res.status === 200){
+            localStorage.removeItem('token');
+            navigate('/login');
+        }
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+    return (
+        <div>UserLogout</div>
+    )
     
 }
 
