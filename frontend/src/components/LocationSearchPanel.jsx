e16094f64cfb2b237cc94bbae60a28148708c2d2
Anmol Upadhyay <anmolupadhau7@gmail.com>
2025-06-02 22:27:07 +0530

    update rides

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -8,25 +8,25 @@ const LocationSearchPanel = () => {
         <div className='w-8 h-8 rounded-full bg-gray-100 flex items-center justify-center'>
           <i className="ri-map-pin-line text-gray-600"></i>
         </div>
-        <h4 className='text-[15px] text-gray-700'>24B, Near Kapoor's Cafe, Sheriyans School, Jaipur</h4>
+        <h4 className='text-[15px] text-gray-700 font-medium'>24B, Near Kapoor's Cafe, Sheriyans School, Jaipur</h4>
       </div>
       <div className='flex items-center gap-3 p-3 hover:bg-gray-100 cursor-pointer'>
         <div className='w-8 h-8 rounded-full bg-gray-100 flex items-center justify-center'>
           <i className="ri-map-pin-line text-gray-600"></i>
         </div>
-        <h4 className='text-[15px] text-gray-700'>Central Park Mall, Shopping District, Malviya Nagar</h4>
+        <h4 className='text-[15px] text-gray-700 font-medium'>Central Park Mall, Shopping District, Malviya Nagar</h4>
       </div>
       <div className='flex items-center gap-3 p-3 hover:bg-gray-100 cursor-pointer'>
         <div className='w-8 h-8 rounded-full bg-gray-100 flex items-center justify-center'>
           <i className="ri-map-pin-line text-gray-600"></i>
         </div>
-        <h4 className='text-[15px] text-gray-700'>Jaipur International Airport, Terminal 2, Airport Road</h4>
+        <h4 className='text-[15px] text-gray-700 font-medium'>Jaipur International Airport, Terminal 2, Airport Road</h4>
       </div>
       <div className='flex items-center gap-3 p-3 hover:bg-gray-100 cursor-pointer'>
         <div className='w-8 h-8 rounded-full bg-gray-100 flex items-center justify-center'>
           <i className="ri-map-pin-line text-gray-600"></i>
         </div>
-        <h4 className='text-[15px] text-gray-700'>Tech Park, Electronic City, Sitapura</h4>
+        <h4 className='text-[15px] text-gray-700 font-medium'>Tech Park, Electronic City, Sitapura</h4>
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
   },[panelOpen])
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className="w-16 absolute top-2 left-2" src='logo.png' alt='logo' ></img>
       <div className='h-screen w-screen'>
         <img className='w-full h-full object-cover' src='https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif'></img>
@@ -79,6 +79,52 @@ const Home = () => {
           </div>
           <div ref={panelRef} className='h-0 overflow-hidden bg-white'>
             <LocationSearchPanel/>
+          </div>
+        </div>
+      </div>
+      <div className='fixed w-full z-10 bottom-0 p-3 bg-white px-3 py-6'>
+        <h3 className='text-2xl font-semibold mb-3'>Select a ride</h3>
+        <div className='flex flex-col gap-3'>
+          <div className='flex p-3 border border-gray-300 active:border-black transition-all duration-200 rounded-xl items-center justify-between cursor-pointer'>
+            <div className='w-24 h-24 flex items-center'>
+              <img className='w-full object-contain' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png'></img>
+            </div>
+            <div className='w-1/2'>
+              <h4 className='text-m font-medium'>LessGoo Cab <span className='ml-2'><i className="ri-user-3-fill"></i>4</span></h4>
+              <h5 className='text-sm font-medium'>2 mins away</h5>
+              <p className='text-xs font-medium text-gray-600'>Affordable & Comfortable Ride</p>
+            </div>
+            <h2 className='text-2xl font-semibold'>
+              ₹198.05
+            </h2>
+          </div>
+
+          <div className='flex p-3 border border-gray-300 active:border-black transition-all duration-200 rounded-xl items-center justify-between cursor-pointer'>
+            <div className='w-24 h-24 flex items-center'>
+              <img className='w-full object-contain' src='https://centralization-images.s3.ap-south-1.amazonaws.com/OLA_Adventure_Right_dbd9705a84.png'></img>
+            </div>
+            <div className='w-1/2'>
+              <h4 className='text-m font-medium'>LessGoo Moto <span className='ml-2'><i className="ri-user-3-fill"></i>2</span></h4>
+              <h5 className='text-sm font-medium'>4 mins away</h5>
+              <p className='text-xs font-medium text-gray-600'>Affordable & Comfortable Ride</p>
+            </div>
+            <h2 className='text-2xl font-semibold'>
+              ₹128.78
+            </h2>
+          </div>
+
+          <div className='flex p-3 border border-gray-300 active:border-black transition-all duration-200 rounded-xl items-center justify-between cursor-pointer'>
+            <div className='w-24 h-24 flex items-center'>
+              <img className='w-full object-contain' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png'></img>
+            </div>
+            <div className='w-1/2'>
+              <h4 className='text-m font-medium'>LessGoo Auto <span className='ml-2'><i className="ri-user-3-fill"></i>3</span></h4>
+              <h5 className='text-sm font-medium'>2 mins away</h5>
+              <p className='text-xs font-medium text-gray-600'>Affordable & Comfortable Ride</p>
+            </div>
+            <h2 className='text-2xl font-semibold'>
+              ₹158.05
+            </h2>
           </div>
         </div>
       </div>
