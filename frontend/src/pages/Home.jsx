2c7350a264ad9e9ed98bae1501a71f4d26ca64dc
Anmol Upadhyay <anmolupadhau7@gmail.com>
2025-06-04 23:10:57 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -95,7 +95,7 @@ const Home = () => {
             </form>
           </div>
           <div ref={panelRef} className='h-0 overflow-hidden bg-white'>
-            <LocationSearchPanel setPanelOpen={} setVehiclePanelOpen={setVehiclePanelOpen}/>
+            <LocationSearchPanel setPanelOpen={setPanelOpen} setVehiclePanelOpen={setVehiclePanelOpen}/>
           </div>
         </div>
       </div>
